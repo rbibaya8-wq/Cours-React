8be0463f1e78536a927998a9e558b63c63337bec
aya rbib <rbibaya8@gmail.com>
2025-11-12 02:23:55 -0800

    feat: add tpEvenemetOnclick project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Client from "./Client";
+import Visiteur from "./Visiteur";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // variable simple
+  const connecte = true; // essaie b false bach tchouf l'autre cas
+
+  // affichage conditionnel
+  if (connecte) {
+    return <Client />;
+  } else {
+    return <Visiteur />;
+  }
 }
 
 export default App;
---
+++ src/Client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Client() {
+  return <h2>Bienvenue !</h2>;
+}
+
+export default Client;
